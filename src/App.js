5a8c59d597412b7a0690ac70a78980d12d66f4b0
Samir Kumar <samir1741012321@gmail.com>
2020-06-02 21:59:33 +0530

    Adding Covid-19 web application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards,Chart, CountryPicker} from './components'
+import styles from './App.module.css'
+import {fetchData} from './api'
+import coronaImage from './images/image.png';
+
+class App extends React.Component {
+	state={				
+								// sending data from here to cards
+		data:{},
+		country:'',
+	}
+	async componentDidMount () {
+		const fetchedData = await fetchData();
+		this.setState({data: fetchedData})
+	}
+	// Chossing country
+	handleCountryChange = async(country)=>{
+		// fetch the data
+		const fetchedData = await fetchData(country);
+		// set the data
+		this.setState({data:fetchedData, country: country})
+
+	}
+	render() {
+		const {data,country}=this.state;
+		return(
+			<div className="{styles.container}">
+				<img className={styles.image} src={coronaImage}alt= "Covid-19"/>
+				<Cards data={data}/>	
+				{/* PASSING DATA AS PROPS TO CARD */}
+				<CountryPicker handleCountryChange={this.handleCountryChange}/>
+				<Chart data={data} country={country}/>
+			</div>
+		)
+	}
 }
-
 export default App;
